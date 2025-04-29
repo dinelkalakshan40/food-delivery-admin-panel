c85762eacd751559786c35f2e37062035411f6c2
dinelka <newlakshan3@gmail.com>
2025-04-29 18:23:34 +0530

    create AddFood form

--- src/component/AddFood.jsx
+++ src/component/AddFood.jsx
@@ -1,7 +1,50 @@
+import {assets} from "../assets/assets.js";
 
 const AddFood=()=>{
-    return(
-        <div>AddFood</div>
+    return (
+        <div className="mx-2 mt-2">
+            <div className="row">
+                <div className="card col-md-4">
+                    <div className="card-body">
+                        <h2 className="mb-4">Add Food</h2>
+                        <form>
+                            <div className="mb-3">
+                                <label htmlFor="image" className="form-label">
+                                    <img src={assets.upload} width={90}/></label>
+                                <input type="file" className="form-control" id="image" required hidden/>
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="name" className="form-label">Food Name</label>
+                                <input type="text" className="form-control" id="name" required name='name'/>
+                            </div>
+
+                            <div className="mb-3">
+                                <label htmlFor="Dsec" className="form-label">Desc</label>
+                                <textarea className="form-control" id="Dsec" rows="5" required name='Desc'></textarea>
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="category" className="form-label">Category</label>
+                                <select name="category" id="category" className='form-control'>
+                                    <option value="Biryani">Biryani</option>
+                                    <option value="Cake">Cake</option>
+                                    <option value="Buger">Buger</option>
+                                    <option value="Pizza">Pizza</option>
+                                    <option value="Rllos">Rolls</option>
+                                    <option value="Sald">Salad</option>
+                                    <option value="Ice cream">Ice cream</option>
+
+                                </select>
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="price" className="form-label">Price</label>
+                                <input type="number" name="price" id="price" className="form-control"/>
+                            </div>
+                            <button type="submit" className="btn btn-primary">Save</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 export default AddFood;

3367848a01483a409b1d08fd3abb3b56dd93052b
dinelka <newlakshan3@gmail.com>
2025-04-29 17:58:27 +0530

    add logo

---
+++ src/assets/assets.js
@@ -0,0 +1,8 @@
+import foodDelivery from './food-delivary.PNG';
+import upload from './upload-food.png'
+
+export const assets ={
+    foodDelivery,
+    upload
+
+};
--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -1,16 +1,17 @@
 import {Link} from "react-router-dom";
+import {assets} from "../assets/assets.js";
 
 const Sidebar = ({sidebarVisible}) => {
     return (
         <div className={`border-end bg-white ${sidebarVisible ? '':'d-none'}`} id="sidebar-wrapper">
-            <div className="sidebar-heading border-bottom bg-light">Start ere</div>
+            <div className="sidebar-heading border-bottom bg-light"><img src={assets.foodDelivery} alt="" height={30} width={30}/></div>
             <div className="list-group list-group-flush">
                 <Link className="list-group-item list-group-item-action list-group-item-light p-3"
-                      to="/add">AddFood</Link>
+                      to="/add"><i className="bi bi-plus-circle me-2"></i>AddFood</Link>
                 <Link className="list-group-item list-group-item-action list-group-item-light p-3"
-                      to="/list">ListFood</Link>
+                      to="/list"><i className="bi bi-list-ul me-2"></i>ListFood</Link>
                 <Link className="list-group-item list-group-item-action list-group-item-light p-3"
-                      to="/orders">Orders</Link>
+                      to="/orders"><i className="bi bi-cart me-2"></i>Orders</Link>
             </div>
         </div>
     )
